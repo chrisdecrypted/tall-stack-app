dc6e763dafab9b356f58d06c8810540d64372abb
Christopher Murray <christopher@christopher-murray.com>
2020-11-09 14:24:10 -0500

    🎉 Create TALL (Tailwinds, Alpine.js, Laravel, Livewire) demo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -25,9 +25,11 @@
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
     </head>
 
     <body>
+        
         @yield('body')
 
         @livewireScripts
