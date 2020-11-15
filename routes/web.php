a5b6d654535f3415a8c6b65d89ab598b7e9d0d39
Christopher Murray <christopher@christopher-murray.com>
2020-11-15 09:39:22 -0500

    🎨 Added TasksController / Resource Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('tasks',TasksController::class);
+
 
 Route::view('/', 'welcome')->name('home');
 
