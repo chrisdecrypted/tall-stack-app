14c051eee7171b4de4e867b96994ed1c6b81aa44
Christopher Murray <christopher@christopher-murray.com>
2020-11-14 23:02:02 -0500

    💄 Improved UI

--- resources/views/livewire/task.blade.php
+++ resources/views/livewire/task.blade.php
@@ -1,67 +1,81 @@
-<body class="bg-gray-200 text-gray-700">
-    <div class="container mx-auto max-w-md px-4 my-4" x-data="todos()">
-        <div class="bg-white shadow-lg rounded px-6 py-4">
-            <p class="text-4xl">My Tasks</p>
-            <ul class="mb-6 rounded border-2 border-gray-100 divide-y divide-solid space-y-2">
+<body class="bg-gray-100 text-gray-700">
+    <div class="container bg-white shadow-xl  rounded-t rounded-b mx-auto max-w-2xl my-14" x-data="todos()">
+        <div class="px-5 py-5">
+            <div class="bg white">
+            <p class="font-semibold text-2xl">My Tasks</p><br>
+            
+            {{-- To-Do --}}
+            <p class="uppercase text-gray-400"> to-do </p>
+            <ul class="mb-6 border rounded-lg border-gray-100 divide-y divide-gray-100 divide-solid space-y-2">
                 <template x-for="todo in todos" :key="todo.id">
-                    <li class="flex w-full justify-between">
+                    <li class="flex w-full justify-between text-gray-400">
                         <div class="flex w-full" :class="{ 'line-through' : todo.isComplete }">
-                            <div class="ml-2 py-2 px-2" x-text="todo.title"></div>
+                            <div class="ml-2 mb-2 py-2 px-2" x-text="todo.title"></div>
                         </div>
-                        <input class="form-checkbox h-6 w-6 text-indigo-600" type="checkbox" x-model="todo.isComplete">
+                        <input
+                            class="form-checkbox h-6 w-6 ml-2 mb-2 mt-2 mr-2 text-teal-600"
+                            type="checkbox" x-model="todo.isComplete">
                     </li>
-                 </div>
-                </div>
-              </template>
-             </ul>
-         </div>
-    <br> New task:
-    <br>
-    <div class="flex w-full">
+                </ul>
+        </div>
+    </template>
+        </div>
 
-        <input type="text" class="shadow-lg w-xl px-2 py-2" x-model="todoTitle" @keydown.enter="addTodo()">
-        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 rounded" x-model="todoTitle"
-            @click="addTodo()"> Add + </button>
+    <p class="uppercase text-gray-400"> completed </p>
+    <ul class="mb-6 border rounded-lg border-gray-100 divide-y divide-gray-100 divide-solid space-y-2">
+        <li class="text-gray-400 ml-2 py-2 px-2 line-through">List Item 1</li>
+
+    </ul></div>
+    <div class="w-full rounded-b bg-gray-100 px-4 py-4">   
+         <p class="uppercase text-gray-400"> new task: </p>
+
+        <div class="flex bg-gray-200 justify-between">
+
+            <input type="text" class="w-4/5" x-model="todoTitle" @keydown.enter="addTodo()" />
+            <button class="w-1/5 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-2 rounded"
+                x-model="todoTitle" @click="addTodo()">Add +</button>
 
         </div>
     </div>
 
-    <script>
-        function todos() {
-            return {
-                todos: [{
-                        id: 1,
-                        title: 'Task 1',
-                        isComplete: false,
-                    },
-                    {
-                        id: 2,
-                        title: 'Task 2',
-                        isComplete: false,
-                    },
-                    {
-                        id: 3,
-                        title: 'Task 3',
-                        isComplete: false,
-                    }
-                ],
-                todoTitle: '',
-                todoId: 4,
-                addTodo() {
-                    if (this.todoTitle.trim() === '') {
-                        return;
-                    }
+</div>
 
-                    this.todos.push({
-                        id: this.todoId,
-                        title: this.todoTitle,
-                        isComplete: false,
-                    })
+        <script>
+            function todos() {
+                return {
+                    todos: [{
+                            id: 1,
+                            title: 'Task 1',
+                            isComplete: false,
+                        },
+                        {
+                            id: 2,
+                            title: 'Task 2',
+                            isComplete: false,
+                        },
+                        {
+                            id: 3,
+                            title: 'Task 3',
+                            isComplete: false,
+                        }
+                    ],
+                    todoTitle: '',
+                    todoId: 4,
+                    addTodo() {
+                        if (this.todoTitle.trim() === '') {
+                            return;
+                        }
 
-                    this.todoId++;
-                    this.todoTitle = '';
-                },
+                        this.todos.push({
+                            id: this.todoId,
+                            title: this.todoTitle,
+                            isComplete: false,
+                        })
+
+                        this.todoId++;
+                        this.todoTitle = '';
+                    },
+                }
             }
-        }
 
-    </script>
+        </script>
