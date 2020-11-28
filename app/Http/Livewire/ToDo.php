dd4602e91da03722e4759c66af2fedadb362bdd8
Christopher Murray <christopher@christopher-murray.com>
2020-11-28 09:22:42 -0500

    New branch created

--- app/Http/Livewire/ToDo.php
+++ app/Http/Livewire/ToDo.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use Illuminate\Http\Request;
 use App\Models\Task;
 
 class ToDo extends Component
@@ -12,8 +11,8 @@ class ToDo extends Component
 
     public function mount()
     {
-        
-        $this->tasks = Task::get(); // returns a collection, NOT a model
+
+        $this->tasks = Task::all(); // returns a collection, NOT a model
         // $this->isDone = $this->tasks->is_done;
 
         $this->pendingTasks = $this->tasks->filter( function($task) {
@@ -29,8 +28,8 @@ class ToDo extends Component
     public function toggle () {
         // $tasks = Task::get();
         // $this->tasks->is_done = true;
-        $tasks = 
-        $tasks->is_done = true; 
+
+        $this->is_done = true;
         return redirect('/');
 
     }
