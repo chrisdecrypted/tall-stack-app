d4c2c350d71b6657cd1622df705c70230d4246c7
Christopher Murray <christopher@christopher-murray.com>
2020-11-29 13:11:59 +0000

    Finally working

--- resources/views/livewire/list-items-pending.blade.php
+++ resources/views/livewire/list-items-pending.blade.php
@@ -1,20 +1,20 @@
 {{-- To-Do --}}
 <div>
-    <p class="uppercase text-gray-400"> To-do
+    <p class="text-gray-400 uppercase"> To-do
    </p>
-    {{ $tasks }}
+  
     @forelse ($tasks as $task)
-<ul class="mb-6 border rounded-lg border-gray-100 divide-y divide-gray-100 divide-solid space-y-2 ">
-        <li class="flex w-full justify-between text-gray-400 ml-2 py-2 px-2">
+<ul class="mb-6 space-y-2 border border-gray-100 divide-y divide-gray-100 rounded-lg divide-solid ">
+        <li class="flex justify-between w-full px-2 py-2 ml-2 text-gray-400">
             <label for="{{ $task -> name }}">{{ $task -> name }}</label>
             <input wire:click="complete({{ $task -> id }})"
                    type="checkbox"
-                   class="form-check-input h-6 w-6 mx-2 my-2 text-teal-600"
+                   class="w-6 h-6 mx-2 my-2 text-teal-600 form-check-input"
                    id="task"/>
             {{ $task -> is_done }}
         </li>
     @empty
-        <li class="flex w-full justify-between text-gray-400 ml-2 py-2 px-2">
+        <li class="flex justify-between w-full px-2 py-2 ml-2 text-gray-400">
             Add a task below to get started
             @endforelse
         </li>
